f5a4a35242edec1c4f03268041e2a5ca9fba77bd
Δανάη Μπρίλλη <danaibrilli@gmail.com>
2021-03-07 13:17:27 +0200

    spin_working_with_popup

--- MyApplication/app/src/main/java/com/alilepidrasi/myapplication/Spin.java
+++ MyApplication/app/src/main/java/com/alilepidrasi/myapplication/Spin.java
@@ -1,8 +1,11 @@
 package com.alilepidrasi.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -16,13 +19,16 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class Spin extends AppCompatActivity {
-    int spin_num = 1;
+    int spin_num = 5;
+    MediaPlayer player;
+    int result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_spin);
         TextView tries = (TextView) findViewById(R.id.triesleft);
+        tries.setText("You have "+ spin_num+" tries left");
         ImageButton spin = (ImageButton) findViewById(R.id.btnwheel);
         spin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,9 +38,10 @@ public class Spin extends AppCompatActivity {
                     tries.setText("You have "+ spin_num+" tries left");
                     Random r = new Random();
                     int degree = 0, degree_old = 0;
-                    int result;
+
                     degree_old = degree % 360;
                     degree = r.nextInt(360) + 720;
+                    result = (degree % 20) * 5;
                     RotateAnimation rotate = new RotateAnimation(degree_old, degree,
                             RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
                     rotate.setDuration(3600);
@@ -44,12 +51,28 @@ public class Spin extends AppCompatActivity {
 
                         @Override
                         public void onAnimationStart(Animation animation) {
-
+                            if (player == null){
+                                player = MediaPlayer.create(getApplicationContext(),R.raw.wheel_sound);
+                                player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                                    @Override
+                                    public void onCompletion(MediaPlayer mp) {
+                                        if (player != null){
+                                            player.release();
+                                            player = null;
+                                        }
+                                    }
+                                });
+                            }
+                            player.start();
                         }
 
                         @Override
                         public void onAnimationEnd(Animation animation) {
-
+                            if (player != null){
+                                player.release();
+                                player = null;
+                                afterAnimation(result);
+                            }
                         }
 
                         @Override
@@ -57,8 +80,9 @@ public class Spin extends AppCompatActivity {
 
                         }
                     });
-                    int result = degree % 360;
+
                     spin.startAnimation(rotate);
+
                 }
                 else{
                     CharSequence text = "No more tries left!!";
@@ -68,5 +92,45 @@ public class Spin extends AppCompatActivity {
             }
         });
 
+    }
+    public void afterAnimation(int result){
+        if (result >= 40 || result ==0) {
+            alertNoPrize();
+        }
+        else{
+            alertPrize();
+        }
+    }
+
+    public void alertPrize(){
+        CharSequence text = "You won a " +result+ "$ coupon from our stores!!";
+        AlertDialog.Builder builder = new AlertDialog.Builder(Spin.this);
+        builder.setTitle("Congratulations!!");
+        builder.setIcon(R.drawable.confetti);
+        builder.setMessage(text);
+        builder.setPositiveButton("Thank you!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+
+    }
+    public void alertNoPrize(){
+        CharSequence text = "Sorry, you didn't win a coupon.Try again next time!";
+        AlertDialog.Builder builder = new AlertDialog.Builder(Spin.this);
+        builder.setTitle("Oh no!");
+        builder.setIcon(R.drawable.sad);
+        builder.setMessage(text);
+        builder.setPositiveButton("Okay!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
